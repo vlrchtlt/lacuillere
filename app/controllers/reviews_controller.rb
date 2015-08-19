86a9dba2221d792139defd05ee1ee323ba158b2c
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-19 17:47:51 +0200

    embeded form for new reviews in restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,26 @@
 class ReviewsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  before_action :find_restaurant, only: [:new, :create]
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = @restaurant.reviews.build(review_params)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
